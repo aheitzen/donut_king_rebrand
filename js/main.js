d6a158db75a7850276ed3cca8c8a358f57406c0d
aheitzen <alexaheitzenrader@yahoo.com>
2016-09-16 12:22:26 -0700

    adding parallax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("I work");
+// $(document).ready(function (){
+// 	$(".landing_page_button").click(function (){
+//     	$('html, body').animate({
+//         	scrollTop: $("#menu_page").offset().top
+//         }, 1000);
+//   	});
+// });
+
+// console.log("I work");
+
+
+
+$(document).ready(function() {
+	$('.parallax-window').parallax({imageSrc: 'images/store-front.jpg'});
+	// $('.parallax-window').parallax({imageSrc: 'images/typewritter.jpeg'})
+	// $('.parallax-window').parallax({imageSrc: 'images/phone.jpeg'});
+});
