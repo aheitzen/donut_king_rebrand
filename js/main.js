eacb46da91999c379ec4b04b9858a21745d4897e
aheitzen <alexaheitzenrader@yahoo.com>
2016-09-16 18:50:56 -0700

    logic and style for first parallax section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// $(document).ready(function (){
-// 	$(".landing_page_button").click(function (){
-//     	$('html, body').animate({
-//         	scrollTop: $("#menu_page").offset().top
-//         }, 1000);
-//   	});
-// });
+$(document).ready(function (){
+	$(".landing_page_button").click(function (){
+    	$('html, body').animate({
+        	scrollTop: $("#coffee-parallax-view").offset().top
+        }, 1000);
+  	});
+});
 
-// console.log("I work");
+
 
 
 
